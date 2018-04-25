ed7d55e5e9b35389daec34d2e1f8a646445de1f8
Thea Tokstad <theatok@stud.ntnu.no>
2018-04-25 10:37:39 +0200

    lagt til popup

--- src/promosjon/view/MaaleapparatKontroller.java
+++ src/promosjon/view/MaaleapparatKontroller.java
@@ -1,10 +1,15 @@
 package promosjon.view;
 
+import java.util.Optional;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 import model.Info;
 
@@ -40,15 +45,24 @@ public class MaaleapparatKontroller {
     
     @FXML
     private void klikkKobleTil()	{
-    	String valgtApparat = list.getSelectionModel().getSelectedItem();
-    	Info.tilkobledeMaaleapparater.set(Info.maaleapparater.indexOf(valgtApparat), true);
-    	kobleTilKlikket = true;
-    	stage.close();
+	    	String valgtApparat = list.getSelectionModel().getSelectedItem();
+	    	Info.tilkobledeMaaleapparater.set(Info.maaleapparater.indexOf(valgtApparat), true);
+	    	kobleTilKlikket = true;
+	    	kobletTil();
+	    	stage.close();
     }
+    
+    private void kobletTil() {
+    	Alert alert = new Alert(AlertType.INFORMATION);
+    		alert.setTitle("Koblet til");
+		alert.setHeaderText("");
+		alert.setContentText("Ditt måleapparat er nå koblet til.");
+		alert.showAndWait();
+	}
     
     @FXML
     private void klikkAvbryt() {
-    	stage.close();
+    		stage.close();
     }
     
 }
